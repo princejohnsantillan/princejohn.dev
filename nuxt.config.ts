8796f12b35609dbc6170c72ff1b4739eba6319bd
Prince John Santillan <mail@princejohnsantillan.net>
2024-05-28 19:57:31 +0800

    remove umami

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -40,16 +40,5 @@ export default defineNuxtConfig({
         'shell',
       ]
     }
-  },
-  app:{
-    head: {
-      script: [
-        {
-          src: 'https://cloud.umami.is/script.js',
-          'data-website-id': process.env.UMAMI_WEBSITE_ID,          
-          defer: true
-        }        
-      ]
-    }
-  }
+  }  
 })
