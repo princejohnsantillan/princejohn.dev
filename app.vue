6a217346e49fe202d2bc08a5d2104d233a460c93
Prince John Santillan <mail@princejohnsantillan.net>
2024-05-28 14:19:09 +0800

    install vercel analytics and speed insights

--- app.vue
+++ app.vue
@@ -1,6 +1,11 @@
 <template>
+    <SpeedInsights />
     <AppLayout>
         <NuxtPage />
     </AppLayout>
 </template>
+
+<script setup lang="ts">
+    import { SpeedInsights } from '@vercel/speed-insights/vue';
+</script>
   
