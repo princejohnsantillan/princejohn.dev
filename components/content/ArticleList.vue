d2939f8d2e8dd1200fb5e620af0b9af6264ae040
Prince John Santillan <mail@princejohnsantillan.net>
2024-05-28 14:10:56 +0800

    minor ui adjustments

--- components/content/ArticleList.vue
+++ components/content/ArticleList.vue
@@ -1,10 +1,12 @@
 <template>
-    <ContentList v-slot="{ list }" :query="query">
-        <div v-for="article in list" :key="article._path">
-            <h3 class="mb-0"><NuxtLink :to="article._path" class="text-primary-500">{{ article.title }}</NuxtLink></h3>
-            <span v-if="article.publish_at" >{{ formatDate(article.publish_at) }}</span>
-        </div>
-    </ContentList>
+    <div class="my-5">
+        <ContentList v-slot="{ list }" :query="query">
+            <div v-for="article in list" :key="article._path">
+                <h4 class="mb-0"><NuxtLink :to="article._path" class="text-primary-500">{{ article.title }}</NuxtLink></h4>
+                <span v-if="article.publish_at" >{{ formatDate(article.publish_at) }}</span>
+            </div>
+        </ContentList>
+    </div>
 </template>
 <script setup lang="ts">
 
